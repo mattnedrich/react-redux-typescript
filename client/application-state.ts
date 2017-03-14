d3e20b4a5dedbfc9c03346ebe695bf75ca4bfe41
Matt Nedrich <mattnedrich@gmail.com>
2017-03-13 22:37:17 -0400

    fixed bug

--- client/application-state.ts
+++ client/application-state.ts
@@ -1,14 +1,14 @@
 class ApplicationState {
+  static getDefault() {
+    return new ApplicationState();
+  }
+
   greeting: string
   count: number;
 
   constructor() {
     this.greeting = 'React-TypeScript-Redux Example';
     this.count = 0;
-  }
-
-  static getDefault() {
-    return new ApplicationState();
   }
 }
 
