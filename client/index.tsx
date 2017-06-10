9be46690d4d789e190cde732139b39917336b88a
Matt Nedrich <mattnedrich@gmail.com>
2017-06-10 18:12:01 -0400

    wired up redux dev tools

--- client/index.tsx
+++ client/index.tsx
@@ -6,7 +6,10 @@ import { createStore } from 'redux';
 import App from './components/app';
 import updateState from './reducers/index';
 
-const store = createStore(updateState);
+const store = createStore(
+  updateState,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__  && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDom.render(
   <Provider store={store}>
